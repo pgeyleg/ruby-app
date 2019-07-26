f1aad9e1a335791332eb94b9851c43859eada112
Pema Geyleg <pema.geyleg@gmail.com>
2019-07-26 09:46:57 -0400

    Installs yarn packages

--- bin/docker-dev-server.sh
+++ bin/docker-dev-server.sh
@@ -4,6 +4,7 @@ set -e
 bundle install --jobs 20 --retry 5
 
 bundle exec rake db:setup
+yarn install --check-files
 
 rm -f tmp/pids/server.pid
 bundle exec rails server -b 0.0.0.0 -p $RAILS_PORT
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
+yarn install --check-files
 
 exec "$@"
 # Finally call command issued to the docker service
